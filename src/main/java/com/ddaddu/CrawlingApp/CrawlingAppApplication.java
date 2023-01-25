0830457588a0bd69531d0e8e05c826121f228ac4
ssongone <41146676+ssongone@users.noreply.github.com>
2023-01-25 22:23:11 +0900

    initial commit

---
+++ src/main/java/com/ddaddu/CrawlingApp/CrawlingAppApplication.java
@@ -0,0 +1,13 @@
+package com.ddaddu.CrawlingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddaddu/CrawlingApp/CrawlingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddaddu.CrawlingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
